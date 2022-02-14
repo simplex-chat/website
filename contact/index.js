a8f4d6a752cf03fdf41bfc4df9bd2069ac01f5d4
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2022-02-14 22:16:34 +0000

    update contact page (#9)
    * redesign contact page
    
    * some improvements
    
    * some more improvements
    
    * add testflight line
    
    * fix margin bottom in section 1
    
    * update contact/index.html
    
    * update contact/index.html
    
    * update layout
    
    * tweak layout
    
    Co-authored-by: M Sarmad Qadeer <MSarmadQadeer@gmail.com>

--- contact/index.js
+++ contact/index.js
@@ -5,7 +5,7 @@
   const mobileConnURIanchor = document.getElementById("mobile_conn_req_uri");
   mobileConnURIanchor.href = connURI.replace("https://simplex.chat", "simplex:");
   if (document.location.pathname.indexOf("/contact") >= 0) {
-    document.querySelector("#conn_req .conn_mode").innerText = "contact address of";
+    document.querySelector("#conn_req .conn_mode").innerText = "address of";
   }
   const els = document.querySelectorAll(".content_copy_with_tooltip");
   if (navigator.clipboard) {
@@ -29,6 +29,8 @@
   function contentCopyWithTooltip(parent) {
     const content = parent.querySelector(".content");
     const tooltip = parent.querySelector(".tooltiptext");
+    console.log(parent.querySelector(".content_copy") ,111)
+    console.log(parent)
     const copyButton = parent.querySelector(".content_copy");
     copyButton.addEventListener("click", copyAddress)
     copyButton.addEventListener("mouseout", resetTooltip)
@@ -43,12 +45,12 @@
     }  
   }
 
-  // function copyAddress() {
-  //   navigator.clipboard.writeText(connURI);
-  //   tooltipEl.innerHTML = "Copied!";
-  // }
+  function copyAddress() {
+    navigator.clipboard.writeText(connURI);
+    tooltipEl.innerHTML = "Copied!";
+  }
 
-  // function resetTooltip() {
-  //   tooltipEl.innerHTML = "Copy to clipboard";
-  // }
+  function resetTooltip() {
+    tooltipEl.innerHTML = "Copy to clipboard";
+  }
 })();
